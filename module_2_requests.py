819dd277338ece7657d7feb759e76500820f36cf
Adelle Arbo <giarbo.28@gmail.com>
2021-11-15 18:18:29 +0800

    Module 2: Help requests

--- module_2_requests.py
+++ module_2_requests.py
@@ -8,42 +8,7 @@ Created on Sun Nov 14 11:35:35 2021
 
 ### Module 2
 
-
-# 2. Create class "Category:
-
-class Category:
-    def __init__(self, name, option):
-        self.name = category
-        self.option = option
-        
-    def OptionsSelect(self, options, name):
-        index = 0
-        indexValidList = []
-        print('Select ' + name + ':')
-    
-        for optionName in options:
-            index = index + 1
-            indexValidList.extend([options[optionName]])
-            print(str(index) + '. ' + options[optionName])
-            inputValid = False
-    
-        while not inputValid:
-            inputRaw = input(name + ': ')
-            inputNo = int(inputRaw) - 1
-        
-            if inputNo > -1 and inputNo < len(indexValidList):
-                selected = indexValidList[inputNo]
-                print('Selected ' +  name + ': ' + selected)
-                inputValid = True
-                break
-            else:
-                print('Please select a valid ' + name + ' number')
-    
-        return selected
-
-
-
-# Sample functions
+from datetime import datetime
 
 # 1. Define OptionsSelect function (for all inputs that ask users to select from list of options)   
 
@@ -75,12 +40,12 @@ def OptionsSelect(options, name):
 
 # 2. Select a Help Category (using OptionsSelect function)
 
-options = {"1": "Errands",
-           "2": "Ride",
-           "3": "Translate",
-           "4": "Tutor"}
+helpCategory = {"1": "Errands",
+                "2": "Ride",
+                "3": "Translate",
+                "4": "Tutor"}
 
-category = OptionsSelect(options, "Help Category")
+hcategory = OptionsSelect(helpCategory, "Help Category")
 
 # 3. Create nested dictionionary for category options
 
@@ -112,6 +77,21 @@ optionsDict = {"Errands": {"1": "Flat maintenance",
 
 # 4. Select option based on selected Help Category 
 
-options = optionsDict[category]
+helpOptions = optionsDict[hcategory]
 
-HelpOption = OptionsSelect(options, "option")
+hoption = OptionsSelect(helpOptions, "option")
+
+# 5. Select date for Help Category 
+
+def validate(date_text):
+    while True:
+        try:
+            date_text == datetime.strptime(date_text, '%Y-%m-%d')
+        except ValueError:
+            raise ValueError("Invalid date. Please enter date in YYYY-MM-DD format.")
+            continue
+        else:
+            break
+
+hdate = str(input("What date do you need help? (yyyy-mm-dd): "))
+validate(hdate)
