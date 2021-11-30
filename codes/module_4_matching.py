ca62c0fdf92f11cae9a3a5b5bbb1c430ab627606
janinepdevera <janinepdevera@gmail.com>
2021-12-01 01:30:46 +0800

    Module 4: Matching (MAIN) - updated

--- codes/module_4_matching.py
+++ codes/module_4_matching.py
@@ -9,9 +9,12 @@ Created on Sat Nov 27 10:15:12 2021
 import pandas as pd
 from geopy.distance import geodesic
 from fuzzywuzzy import fuzz
+from datetime import datetime 
+from datetime import date 
 
-rdb = pd.read_csv("./data/requests_database.csv")
-sdb = pd.read_csv("./data/support_database.csv")
+
+rdb = pd.read_csv("../data/requests_database.csv")
+sdb = pd.read_csv("../data/support_database.csv")
 
 # MATCHING
 
@@ -23,21 +26,55 @@ for df in df_list:
     df["string"] = (df["Cat"] + df["Opt"].fillna('') +
                     df["AddInfo"].fillna('')).astype(str)
 
-# 2. Generate text similarity scores (rdb vs sdb)
+# 2. Prepare dataframe of all helper-helpee pairs 
 
 matches_cols = ["rdb_transactionID", "rdb_userID", "rdb_username", "rdb_string",
                 "sdb_transactionID", "sdb_userID", "sdb_username", "sdb_string"]
 matches_df = pd.DataFrame(columns = matches_cols)
 
-scores_df = pd.DataFrame(columns = ["textScores"])
+scores_df = pd.DataFrame(columns = ["textScores", "DateDiff", "TimeDiff",
+                                    "orgDistance"])
+
+# 3. LEVEL 1: Generate text similarity scores (for each pair)
 
 for r in range(len(rdb)):
+    
     scores = []
+    dates = []
+    times = []
+    orgDist = []
+    destDist = []
+    
     for s in range(len(sdb)):
         score = fuzz.partial_ratio(rdb.string[r].lower(), sdb.string[s].lower())
         scores.append(score)
 
-# 3. Create dataframe of all helper-helpee pairs  
+# 4. LEVEL 2: Generate date difference and time difference (for each pair)   
+  
+        # remove time difference
+        date = datetime.strptime(rdb.Date[r], '%Y-%m-%d') - datetime.strptime(sdb.Date[s], '%Y-%m-%d')
+        dates.append(date)
+        
+        # remove day difference
+        time = datetime.strptime(rdb.Time[r], '%H:%M:%S') -  datetime.strptime(sdb.Time[s], '%H:%M:%S')
+        times.append(time)
+
+# 5. LEVEL 3: Generate location (origin) differences (for each pair)
+
+        a = rdb.OrgCoord[r].strip('()').split(',')
+        a = tuple(a)
+        a = (float(a[0]), float(a[1]))
+        
+        b = sdb.OrgCoord[s].strip('()').split(',')
+        b = tuple(b)
+        b = (float(b[0]), float(b[1]))
+
+        orgDist.append(geodesic(a, b).kilometers)
+        
+# 6. LEVEL 3: Generate location (destination) differences (for each pair)
+
+# 7. Populate helper-helpee dataframe with scores
+
         matches_df = matches_df.append({'rdb_transactionID':rdb.transactionID[r],
                                         'rdb_userID':rdb.userID[r],
                                         'rdb_username':rdb.username[r],
@@ -47,19 +84,27 @@ for r in range(len(rdb)):
                                         'sdb_username':sdb.username[s],
                                         'sdb_string':sdb.string[s]},
                                         ignore_index=True)
-      
-    scores_df = scores_df.append({'textScores':scores},
-                                    ignore_index=True)
         
-    scores_df = scores_df.explode('textScores')
+    scores_df = scores_df.append({'textScores':scores,
+                                 'DateDiff': dates,
+                                 'TimeDiff': times,
+                                 'orgDistance': orgDist},
+                                 ignore_index=True)
+        
+    scores_df = scores_df.explode(['textScores', 'DateDiff', 'TimeDiff', 'orgDistance'])
     
-match1 = pd.concat([matches_df.reset_index(drop=True), 
+match_final = pd.concat([matches_df.reset_index(drop=True), 
                     scores_df.reset_index(drop=True)], 
                    axis=1)
 
-            
+# 8. Match using text score, date and time difference, OD differences            
 
-    
+# 9. Print details of match    
+
+
+
+
+
 ### OLD CODES ###
 
 #scores_list = []
@@ -67,7 +112,7 @@ match1 = pd.concat([matches_df.reset_index(drop=True),
 #match_string = []
 
 #for r in range(len(rdb)):
-    scores = []
+    #scores = []
     #for s in range(len(sdb)):
         #score = fuzz.ratio(rdb.string[r], sdb.string[s])
         # create list of scores for each obs
@@ -108,40 +153,3 @@ match1 = pd.concat([matches_df.reset_index(drop=True),
       #"\n Thank you for using GetHelp!"
       #)
 
-# Matching with distance:
-# DO NOT RUN
-
-#rdb_Ride = rdb.loc[rdb["Cat"] == "Ride"]
-#sdb_Ride = sdb.loc[sdb["Cat"] == "Ride"]
-
-
-# for r in range(len(rdb_Ride)):
-#scores = []
-#distDiff = []
-# for s in range(len(sdb_Ride)):
-#score = fuzz.ratio(rdb_Ride.string[r],sdb_Ride.string[s])
-# scores.append(score)
-
-#diff = float(rdb_Ride.distanceKM[r]) - float(sdb_Ride.distanceKM[s])
-# distDiff.append(diff)
-
-# 2. Create distance variable
-#distance =[]
-
-# for i in range(len(df)):
-# if isinstance(df.OrgCoord[i], float) == True:
-# distance.append("NA")
-
-# else:
-
-#a = df.OrgCoord[i].strip('()').split(',')
-#a = tuple(a)
-#a = (float(a[0]), float(a[1]))
-
-#b = df.DestCoord[i].strip('()').split(',')
-#b = tuple(b)
-#b = (float(b[0]), float(b[1]))
-
-#distance.append(geodesic(a, b).kilometers)
-
-#df["distanceKM"] = distance
