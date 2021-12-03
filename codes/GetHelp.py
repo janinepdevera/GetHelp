7ef643b634ae1c640d2a50cb07186ba4289c4f5f
Adelle Arbo <giarbo.28@gmail.com>
2021-12-03 01:18:58 +0100

    Run GetHelp :)

---
+++ codes/GetHelp.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  2 19:52:10 2021
+
+@author: admin
+"""
+
+import module_1_login_oop as m1
+import module_2_requests_oop as m2
+import module_3_save_data as m3
+import module_4_matching_oop_AA as m4
+
+
+Match = m4.Matches()
+
+def GetHelp():
+    action = str(input("Guten Tag! Welcome to GetHelp!" +
+              "\nAre you here to: " + 
+              "\n1. Add Request/Offer" +
+              "\n2. Check status of Request/Offer?" + 
+              "\nEnter 1 or 2: "))
+    
+    if action == "1":
+        m3.saveData()
+    elif action == "2":      
+        Match.runAll()
+    else:
+        print("\nPlease enter a valid option.")
+        GetHelp()
+        
+GetHelp()
+
+
+
